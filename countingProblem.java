487779f1215bb021b8278ee06044815e2f2e8971
Keshav Shakti Singh <keshavshakti14@gmail.com>
2022-11-30 22:27:53 +0530

    done

--- countingProblem.java
+++ countingProblem.java
@@ -1,7 +1,6 @@
 /* package codechef; // don't place package name! */
 
 import java.util.*;
-import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
@@ -9,8 +8,7 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	try{
-	Scanner s
-    c = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	int test = sc.nextInt();
 	while(test-->0){
 	    int num = sc.nextInt();
